fb8d0a0b5820bb8d2fec25a714666b090a43f66e
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2020-08-28 17:22:03 -0300

    Injectable dll memory sharing code

--- injectable/injectable.cpp
+++ injectable/injectable.cpp
@@ -1,5 +1,19 @@
 #include <Windows.h>
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+  if (fdwReason == DLL_PROCESS_ATTACH) {
+    mod = hinstDLL;
+    connection.rPipe    = CreateThread(nullptr, 0, MainThread, nullptr, CREATE_SUSPENDED, nullptr);
+    MODULEINFO modInfo;
+    GetModuleInformation(GetCurrentProcess(), mod, &modInfo, sizeof(MODULEINFO));
+    static void *backup[2];
+    void        **entry = static_cast<void **>(modInfo.EntryPoint) + 1000;
+    DWORD       _old;
+    VirtualProtect(entry, sizeof(backup), PAGE_EXECUTE_READWRITE, &_old);
+    backup[0] = entry[0];
+    backup[1] = entry[1];
+    entry[0]  = &connection;
+    entry[1]  = &backup;
+  }
   return true;
 }
