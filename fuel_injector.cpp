7bb95c7a71a906f473b0084336d63fc172c75adb
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2020-08-28 17:21:12 -0300

    Injector code
    - Injects DLL and create a duplex pipe
    - Unbind and send data
    - Send data

--- fuel_injector.cpp
+++ fuel_injector.cpp
@@ -1,12 +1,60 @@
+#include <Windows.h>
 #include "fuel_injector.hpp"
+
+#include <psapi.h>
 
 #include "injectable.hpp"
 
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <functional>
+#include <string>
+#include <vector>
 
-#include <Windows.h>
+static const std::string dllName = "fuel.dll";
+
+static const std::string &getDllPath() {
+  static bool        flag = true;
+  static std::string dllPath;
+  if (flag) {
+    flag = false;
+    dllPath.resize(MAX_PATH + 1);
+    dllPath.resize(GetTempPathA(dllPath.size(), &dllPath[0]));
+    dllPath.append(dllName);
+  }
+  return dllPath;
+}
+
+class Memory {
+  HANDLE h;
+  size_t size;
+  void   *addr;
+public:
+  Memory(HANDLE h, size_t size, const void *data = nullptr) : h(h), size(size) {
+    addr = VirtualAllocEx(h, nullptr, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (data != nullptr) {
+      (*this)(data);
+    }
+  }
+
+  void operator()(const void *data, size_t dataSize = -1, size_t offset = 0) {
+    WriteProcessMemory(h, static_cast<char *>(addr) + offset, data, dataSize == -1 ? size : dataSize, nullptr);
+  }
+
+  operator void *() const { // NOLINT(google-explicit-constructor)
+    return addr;
+  }
+
+  void release() {
+    if (addr != nullptr) {
+      VirtualFreeEx(h, addr, size, MEM_RELEASE);
+      addr = nullptr;
+    }
+  }
+
+  ~Memory() {
+    release();
+  }
+};
 
 FuelInjector::FuelInjector(DWORD pid) : pid(pid) {
 }
@@ -16,33 +64,103 @@ FuelInjector::~FuelInjector() {
 }
 
 bool FuelInjector::Bind() {
-  if(handle != nullptr) {
+  if (pHandle != nullptr) {
     return false;
   }
-  handle = OpenProcess(PROCESS_ALL_ACCESS, false, pid); // NOLINT(hicpp-signed-bitwise)
-  if(handle == nullptr) {
+  pHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid); // NOLINT(hicpp-signed-bitwise)
+  if (pHandle == nullptr) {
     return false;
   }
 
-  std::string dllPath;
-  dllPath.resize(MAX_PATH + 1);
-  dllPath.resize(GetTempPathA(dllPath.size(), &dllPath[0]));
-  dllPath.append("fuel.dll");
+  void *infoAddr;
+  {
+    const auto &dllPath = getDllPath();
 
-  std::ofstream ofs;
-  ofs.open(dllPath, std::ios::trunc | std::ios::binary);
-  if(ofs.is_open()) {
-    ofs.write(Injectable::data, Injectable::size);
-    ofs.close();
+    {
+      std::ofstream ofs;
+      ofs.open(dllPath, std::ios::trunc | std::ios::binary);
+      if (ofs.is_open()) {
+        ofs.write(Injectable::data, Injectable::size);
+        ofs.close();
+      }
+    }
+
+    {
+      Memory m(pHandle, dllPath.size() + 1, dllPath.c_str());
+      HANDLE th = CreateRemoteThread(pHandle, nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(&LoadLibraryA), m, 0, nullptr);
+      WaitForSingleObject(th, INFINITE);
+      CloseHandle(th);
+    }
+
+    {
+      DWORD requiredSize;
+      EnumProcessModules(pHandle, nullptr, 0, &requiredSize);
+      std::vector<HMODULE> modules;
+      modules.resize(requiredSize / sizeof(HMODULE));
+      EnumProcessModules(pHandle, modules.data(), requiredSize, &requiredSize);
+      for (HMODULE &mod : modules) {
+        char name[255];
+        GetModuleBaseNameA(pHandle, mod, name, 255);
+        if (dllName == name) {
+          module = mod;
+          break;
+        }
+      }
+      if (module == nullptr) {
+        Unbind();
+        return false;
+      }
+
+      MODULEINFO modInfo;
+      GetModuleInformation(pHandle, module, &modInfo, sizeof(MODULEINFO));
+      infoAddr = static_cast<void **>(modInfo.EntryPoint) + 1000;
+    }
+
   }
+
+  {
+    void *entryData[2];
+    ReadProcessMemory(pHandle, infoAddr, &entryData, sizeof(entryData), nullptr);
+    void *remoteAddr = entryData[0];
+    void *backupAsm  = entryData[1];
+    ReadProcessMemory(pHandle, backupAsm, &entryData, sizeof(entryData), nullptr);
+    WriteProcessMemory(pHandle, infoAddr, &backupAsm, sizeof(void *) * 2, nullptr);
+    DWORD _old;
+    VirtualProtectEx(pHandle, infoAddr, sizeof(void *) * 2, PAGE_EXECUTE, &_old);
+    infoAddr = remoteAddr;
+  }
+
+  ReadProcessMemory(pHandle, infoAddr, &remote, sizeof(remote), nullptr);
+
+  remoteThread = remote.rPipe;
+  DuplicateHandle(pHandle, remoteThread, GetCurrentProcess(), &remoteThread, 0, false, DUPLICATE_SAME_ACCESS);
+
+  CreatePipe(&rPipe, &remote.wPipe, nullptr, 4096);
+  CreatePipe(&remote.rPipe, &wPipe, nullptr, 4096);
+  DuplicateHandle(GetCurrentProcess(), remote.wPipe, pHandle, &remote.wPipe, 0, false, DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS);
+  DuplicateHandle(GetCurrentProcess(), remote.rPipe, pHandle, &remote.rPipe, 0, false, DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS);
+
+  WriteProcessMemory(pHandle, infoAddr, &remote, sizeof(remote), nullptr);
+
+  ResumeThread(remoteThread);
 
   return true;
 }
 
 void FuelInjector::Unbind() {
-  if(handle == nullptr) {
+  if (pHandle == nullptr) {
     return;
   }
-  CloseHandle(handle);
-  handle = nullptr;
+  /*if (module != nullptr) {
+    HANDLE th = CreateRemoteThread(pHandle, nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(&FreeLibrary), module, 0, nullptr);
+    WaitForSingleObject(th, INFINITE);
+    CloseHandle(th);
+  }*/
+  CloseHandle(pHandle);
+  pHandle = nullptr;
+}
+
+void FuelInjector::Send(const char* data, size_t size) {
+  DWORD w;
+  WriteFile(wPipe, data, size, &w, nullptr);
 }
--- fuel_injector.hpp
+++ fuel_injector.hpp
@@ -2,18 +2,29 @@
 
 #ifndef BASETYPES
 typedef unsigned long DWORD;
-typedef void          *HANDLE;
+
+typedef void *HANDLE;
+typedef void *HINTANCE;
+typedef void *HMODULE;
 #endif
 
 class FuelInjector {
-  DWORD  pid;
-  HANDLE handle = nullptr;
+  DWORD   pid;
+  HANDLE  remoteThread{}, pHandle{};
+  HANDLE  rPipe{}, wPipe{};
+  HMODULE module{};
+
+  struct {
+    HANDLE rPipe{}, wPipe{};
+  }       remote{};
 public:
-  FuelInjector(DWORD pid);
+  explicit FuelInjector(DWORD pid);
 
   ~FuelInjector();
 
   bool Bind();
 
   void Unbind();
+
+  void Send(const char *data, size_t size);
 };
